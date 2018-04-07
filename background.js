40858a9905a0802840f1b575ec0950704ca0a8ca
rogojagad <rogojagadalit@gmail.com>
2018-04-07 20:18:07 +0700

    notification for next event

--- background.js
+++ background.js
@@ -1,46 +1,91 @@
 function loadKegiatan(){
-    $.ajax({
-      url: "http://reservasi.lp2.if.its.ac.id/feeder/Laboratorium%20Pemrograman%202",
-      type: 'GET',
-      success: function (data) {
-        if(data.now.length > 0){
-          next_event = data.now[0]
+		$.ajax({
+			url: "http://reservasi.lp2.if.its.ac.id/feeder/Laboratorium%20Pemrograman%202",
+			type: 'GET',
+			success: function (data) {
+				if(data.next.length > 0){
+					next_event = data.next[0]
 
-          var message = {
-            "nama": next_event.nama_kegiatan,
-            "mulai": next_event.waktu_mulai_permohonan_peminjaman,
-            "selesai": next_event.waktu_selesai_permohonan_peminjaman
-          }
+					var message = {
+						"nama": next_event.nama_kegiatan,
+						"mulai": next_event.waktu_mulai_permohonan_peminjaman,
+						"selesai": next_event.waktu_selesai_permohonan_peminjaman
+					}
 
-          makeNotification(message);
-          sendMessage(message);
+					sendMessage("ada", message);
+
+					notify(message);
+
+					setState('disabled');
+				}
+				else {
+					var message = {
+						"nama": null,
+						"mulai": null,
+						"selesai": null
+					}
+
+					sendMessage("tidak", message);
+
+					setState('enabled');
+				}
+			}
+		});
+
+		chrome.storage.local.get(['status'], function(result){
+			console.log(result);
+		});
+}
+
+function sendMessage(bool, kegiatan) {
+	var views = chrome.extension.getViews({type: "popup"});
+
+	if (bool === "ada") {
+		var info_kegiatan = "<p style='text-align: center; margin-top: 4em;'><strong>"+kegiatan.nama+"</strong></p>";
+		info_kegiatan += "<p style='text-align: center'>"+kegiatan.mulai+" sampai "+kegiatan.selesai+"</p>"
+	}
+	else{
+		var info_kegiatan = "<p style='text-align:center;margin-top: 5em;'>Belum ada, enjoy your time :)</p>";
+	}
+
+	if (views.length !== 0) {
+		views[0].document.getElementById('container').innerHTML = info_kegiatan;
+	}
+}
+
+function notify(kegiatan){
+	chrome.storage.local.get(['status'], function(result){
+		if(result.status === "enabled"){
+
+			var notification = new Notification("Kegiatan Berikutnya", {
+				icon: 'icon.png',
+				body: kegiatan.nama + " akan segera dimulai pada pukul "+kegiatan.mulai+". Segera sterilkan laboratorium.",
+				requireInteraction: true
+			});
+
+			notification.create;
+
+			setState('disabled');
+		}
+	});
+}
+
+function setState(state){
+	chrome.storage.local.set({'status': state}, function() {
+		console.log('state is '+state);
+	});
+}
+
+var initialize = (function() {
+    var executed = false;
+    return function() {
+        if (!executed) {
+            executed = true;
+            setState('enabled');
         }
-      }
-    });
-}
+    };
+})();
 
-setInterval(loadKegiatan, 1000);
+initialize();
 
-function sendMessage(kegiatan) {
-  var views = chrome.extension.getViews({type: "popup"});
-
-  var info_kegiatan = "<p style='text-align: center; margin-top: 4em;'><strong>"+kegiatan.nama+"</strong></p>";
-  info_kegiatan += "<p style='text-align: center'>"+kegiatan.mulai+" sampai "+kegiatan.selesai+"</p>"
-
-  views[0].document.getElementById('container').innerHTML = info_kegiatan;
-}
-
-function makeNotification(kegiatan){
-  var option = {
-    type: "basic",
-    title: "Kegiatan Berikutnya",
-    message: kegiatan.nama + " akan dimulai pukul " + kegiatan.mulai + ". Segera sterilkan lab.",
-    iconUrl: "icon.png"
-  }
-
-  chrome.notifications.create(option, callback);
-}
-
-function callback(){
-  console.log("Done");
-}
+setInterval(loadKegiatan, 10000);
